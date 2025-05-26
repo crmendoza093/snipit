f688d75b13f623bf5496d17fed2030be8757af1e
Cristian Mendoza <cmendoza@buk.co>
2025-05-26 15:22:53 -0500

    feat: configurar gema geocoder

---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,15 @@
+Geocoder.configure(
+  timeout: 5,                  # Tiempo de espera para geocoder (en segundos)
+  lookup: :ipinfo_io,          # Servicio de geocodificación
+  ip_lookup: :ipinfo_io,       # Servicio para búsqueda de IP
+  language: :es,               # Idioma preferido para los resultados
+  use_https: true,             # Usar HTTPS para las solicitudes
+  
+  # Manejo de errores
+  always_raise: [              
+    Geocoder::OverQueryLimitError,
+    Geocoder::RequestDenied,
+    Geocoder::InvalidRequest,
+    Geocoder::InvalidApiKey
+  ],
+) 
